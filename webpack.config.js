51e169e9e9faadf513a2649787dea4c27c99d7b1
tigriscodes <walidkurchied@Walids-MacBook-Pro.local>
2018-06-05 00:10:54 +0100

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
                 exclude: /node_modules/,
                 loader: "babel-loader",
                 options: {
-                  presets: ['es2015', 'react']
+                  presets: ['react']
                 }
             },
             {
