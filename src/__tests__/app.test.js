eed7e4c1ec96a24fbbe39ad4c67ec856cbd29d27
Walid Kurchied <WalidKurchied@users.noreply.github.com>
2020-12-14 23:36:29 +0000

    migrated website to typescript

---
+++ src/__tests__/app.test.js
@@ -0,0 +1,7 @@
+const App = require('../components/App');
+// const ReactTestRenderer = require('react-test-renderer');
+
+describe('<App />', () => {
+  it('should render a div with id site-container', () => {
+  });
+});
